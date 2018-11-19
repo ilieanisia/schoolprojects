c05e83d33cb0cb4426ed13ccaf87ddd1cf226386
ilieanisia <44974996+ilieanisia@users.noreply.github.com>
2018-11-19 17:23:49 +0200

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+unsigned int ziua,luna,anul;
+unsigned cod;
+int main()
+{
+   cout<<"Zi = ";cin>>ziua;
+   cout<<"Luna = ";cin>>luna;
+   cout<<"Anul = ";cin>>anul;
+   if(zi=19) {cout<<"10011";}
+   if(luna=11) {cout<<"1011";}
+   if(an=2018) {cout<<"11111100010";}
+   cout<<" Codul datei este = 10011.1011.11111100010 " <<10011101100000000000011111100010;
+       else{<<"2642413538">>}
+
+
+
+return 0;}
+

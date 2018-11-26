f9637ecaa9a61daa86b45b33eda92549faf0c2d9
ilieanisia <44974996+ilieanisia@users.noreply.github.com>
2018-11-26 17:53:26 +0200

    Add files via upload

--- 5.cpp
+++ 5.cpp
@@ -1,19 +1,48 @@
 #include<iostream>
 using namespace std;
-unsigned int ziua,luna,anul;
-unsigned cod;
+unsigned int n;
+unsigned int ziua;
+unsigned int luna;
+unsigned int anul;
 int main()
 {
-   cout<<"Zi = ";cin>>ziua;
-   cout<<"Luna = ";cin>>luna;
-   cout<<"Anul = ";cin>>anul;
-   if(zi=19) {cout<<"10011";}
-   if(luna=11) {cout<<"1011";}
-   if(an=2018) {cout<<"11111100010";}
-   cout<<" Codul datei este = 10011.1011.11111100010 " <<10011101100000000000011111100010;
-       else{<<"2642413538">>}
+cout<<" Day = ";cin>>ziua;
+cout<<" Month = ";cin>>luna;
+cout<<" Year = ";cin>>anul;
+cout<<endl<<" (dd/mm//yy) = 1 ; (mm/dd/yy) =2 ";
+int code;
+cin>>code;
+if (code==1) {
+                     cout<<endl<<" French format "<<endl;
+                     // ...
+                      ziua=ziua<<27;
+                      n=n|ziua;
+                      luna=luna<<23;
+                      n=n|luna;
+                      n=n|anul;
+                     cout<<n;
+                   return 0; }
+    else
+if (code==2)  {
+                     cout<<endl<<" English format "<<endl;
+                     // ...
+                        anul=anul<<21;
+                        n<<n|anul;
+                        luna=luna<<17;
+                        n=n|luna;
+                        n=n|ziua;
+
+                     cout<<n;
+           return 0;   }
+
+else
+{ cout<<endl<<" Sorry , no banans today !"; }
 
 
 
 return 0;}
+
+
+
+
 
